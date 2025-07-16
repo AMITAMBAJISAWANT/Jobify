569ff30b0623ce74c67f3206c63f8d97c951e0e2
Amit <amitsavant43@gmail.com>
2025-07-16 18:29:57 +0530

    minor changes in validation testing

--- server.js
+++ server.js
@@ -27,16 +27,16 @@ app.get('/',(req,res)=>{
 
 app.post('/api/v1/test',[body('name').notEmpty('name')
                           .withMessage('name is required')
-],(res,res,next)=>{
+],(res,req,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         const errorMessage = errors.array.map((error) => error.msg);
-     
-
-
+        return res.status(400).json({ errors:errorMessage })
     }
+    next();
     
-},(req,res)=>{
+},
+(req,res)=>{
     res.json({message:"First app",data:req.body});
 });
 
